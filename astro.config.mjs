f8b869574ce6d10bc54afc8b4e53736cab9c1602
Lillian497 <luhomeju@gmail.com>
2025-07-06 22:12:15 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	output: "static",
-	site: "https://fuwari.vercel.app/",
+	site: "https://fuwari-1.vercel.app/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
