2bf65fcf5ea1dfc0ef4ac6a296fdd0511b49d78f
Lillian497 <luhomeju@gmail.com>
2025-07-07 22:18:45 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
+	title: "程式及實驗學習日記",
+	subtitle: "歡迎來到我的學習筆記，我記錄了自己的物理研究所學到之基礎知識，以及程式的解題日誌！",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
